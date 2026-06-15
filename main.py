570057f4f9d1b9a74ef9f6e51143b67648054840
grienaylouis-dev <grienaylouis@gmail.com>
2026-06-15 21:38:38 +0200

    A: Initialisation (black screen)

---
+++ main.py
@@ -0,0 +1,19 @@
+import pygame
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from logger import log_state
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            pass
+        screen.fill("black")
+
+        pygame.display.flip() #Leave it last
+
+
+if __name__ == "__main__":
+    main()

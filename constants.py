555bed760d014be2450bd85714ba79febb60fd68
grienaylouis-dev <grienaylouis@gmail.com>
2026-06-15 23:16:29 +0200

    D: CH3 L4 moving the player

--- constants.py
+++ constants.py
@@ -4,3 +4,4 @@ SCREEN_HEIGHT = 720
 PLAYER_RADIUS = 20 #radius of the player ship
 LINE_WIDTH = 2 #width of the line that draw the player ship
 PLAYER_TURN_SPEED = 300
+PLAYER_SPEED = 200
--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 import pygame
 from circleshape import CircleShape #extension of the base Sprite class
-from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -28,8 +28,17 @@ class Player(CircleShape):
     def update(self, dt: float) -> None:
         keys = pygame.key.get_pressed()
 
+        if keys[pygame.K_z]:
+            self.move(dt)
         if keys[pygame.K_q]:
             self.rotate(-dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
+
+    def move(self, dt: float) -> None:
+        unit_vector = pygame.Vector2(0, 1)
+        rotated_vector = unit_vector.rotate(self.rotation)
+        self.position += rotated_vector * PLAYER_SPEED * dt
 
